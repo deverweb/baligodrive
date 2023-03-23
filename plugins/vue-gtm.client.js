fd61494ac4f12be815e77af388c95429c5c80287
deverweb <andreydever@gmail.com>
2023-03-24 03:51:20 +0400

    sdasda

--- plugins/vue-gtm.client.js
+++ plugins/vue-gtm.client.js
@@ -3,8 +3,8 @@ import { createGtm } from "@gtm-support/vue-gtm";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(
     createGtm({
-      // id: "GTM-KS3FNZS",
-      id: "GTM-NFTQCJ9",
+      id: "GTM-KS3FNZS", // PROD
+      // id: "GTM-NFTQCJ9", // DEV
       defer: false,
       compatibility: false,
       enabled: true,
